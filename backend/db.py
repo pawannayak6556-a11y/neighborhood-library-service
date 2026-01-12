79485185a7ddf62e9a297b8955dab1b2b91026a9
pawannayak6556-a11y <pawan.nayak.6556@gmail.com>
2026-01-12 10:15:43 +0530

    Update db.py

--- backend/db.py
+++ backend/db.py
@@ -1,1 +1,10 @@
+import psycopg2
+from config import DB_HOST, DB_NAME, DB_USER, DB_PASS
 
+def get_conn():
+    return psycopg2.connect(
+        host=DB_HOST,
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASS
+    )
