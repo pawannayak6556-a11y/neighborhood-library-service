7199d5f7b0bbf9e0ca962ad1f55567c0ca13239d
pawannayak6556-a11y <pawan.nayak.6556@gmail.com>
2026-01-12 10:20:51 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,1 +1,30 @@
+import { useEffect, useState } from "react";
+import { getBooks, createBook } from "./api/libraryApi";
 
+function App() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    getBooks().then(setBooks);
+  }, []);
+
+  return (
+    <div>
+      <h2>Library</h2>
+      <button onClick={() =>
+        createBook({ title: "1984", author: "Orwell", copies: 2 })
+          .then(() => getBooks().then(setBooks))
+      }>
+        Add Book
+      </button>
+
+      <ul>
+        {books.map(b => (
+          <li key={b.id}>{b.title} ({b.available_copies})</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
