336921e01863064ca74181572b9bdd1b6caf44b6
pawannayak6556-a11y <pawan.nayak.6556@gmail.com>
2026-01-12 10:20:09 +0530

    Update libraryApi.js

--- frontend/src/api/libraryApi.js
+++ frontend/src/api/libraryApi.js
@@ -1,1 +1,11 @@
+const BASE = "http://localhost:3001";
 
+export const getBooks = () =>
+  fetch(`${BASE}/books`).then(res => res.json());
+
+export const createBook = (data) =>
+  fetch(`${BASE}/books`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data)
+  });
