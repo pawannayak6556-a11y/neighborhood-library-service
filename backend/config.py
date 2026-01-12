de0735435fb49d415957042cb6a8ef5d82d47d2d
pawannayak6556-a11y <pawan.nayak.6556@gmail.com>
2026-01-12 10:15:14 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,1 +1,10 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_NAME = os.getenv("DB_NAME", "library")
+DB_USER = os.getenv("DB_USER", "admin")
+DB_PASS = os.getenv("DB_PASS", "admin")
+GRPC_PORT = os.getenv("GRPC_PORT", "50051")
